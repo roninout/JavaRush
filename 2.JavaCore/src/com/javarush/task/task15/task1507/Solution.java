4e65dcffd45b5747868b5260ab3aecf2783d8860
roninout <roninorgua@gmail.com>
2017-03-23 16:52:31 +0200

    03.03.2017

--- 2.JavaCore/src/com/javarush/task/task15/task1507/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1507/Solution.java
@@ -9,12 +9,94 @@ public class Solution {
         printMatrix(2, 3, "8");
     }
 
+    // 1
     public static void printMatrix(int m, int n, String value) {
         System.out.println("Заполняем объектами String");
         printMatrix(m, n, (Object) value);
     }
 
+    // 2
     public static void printMatrix(int m, int n, Object value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+    // 3
+    public static void printMatrix(Integer m, int n, Object value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+    // 4
+    public static void printMatrix(int m, Integer n, Object value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+    // 5
+    public static void printMatrix(Integer m, Integer n, Object value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+    // 6
+    public static void printMatrix(char m, int n, Object value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+    // 7
+    public static void printMatrix(int m, char n, Object value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+    // 8
+    public static void printMatrix(char m, char n, Object value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+    // 9
+    public static void printMatrix(Integer m, Integer n, String value) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+    // 10
+    public static void printMatrix(char m, char n, String value) {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(value);
