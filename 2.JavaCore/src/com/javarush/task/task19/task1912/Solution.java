bc46ae9a7095debbe4acf8ca1c38fe7a6c7a7a10
roninout <roninorgua@gmail.com>
2017-04-10 18:26:19 +0300

    05.04.2017

--- 2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
@@ -4,10 +4,26 @@ package com.javarush.task.task19.task1912;
 Ридер обертка 2
 */
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+
+        System.setOut(printStream);
+        testString.printSomething();
+
+        String result = outputStream.toString();
+
+        System.setOut(consoleStream);
+
+        System.out.println(result.replace("te", "??"));
     }
 
     public static class TestString {
