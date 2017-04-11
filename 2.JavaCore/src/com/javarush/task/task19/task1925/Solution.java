bcb822a9c3e2ba30a70e9d163882d647d611fcd9
alexanderkovalchuk <roninorgua@gmail.com>
2017-04-11 22:54:01 +0300

    2017_04_10

--- 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
@@ -8,6 +8,33 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Solution {
+    public static void main(String[] args) throws IOException
+    {
+        String firstFile = args[0];
+        String secondFile = args[1];
+
+        BufferedReader reader = new BufferedReader(new FileReader(firstFile));
+        FileWriter writer = new FileWriter(secondFile);
+
+        String line, result="";
+        while ((line = reader.readLine()) != null)
+        {
+            String [] array = line.split(" ");
+            for (String str : array)
+            {
+                if (str.length() > 6) result += str + ",";
+            }
+        }
+        writer.write(result.substring(0, result.length() - 1));
+        reader.close();
+        writer.close();
+
+    }
+}
+
+
+/*
+public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader fileReader = new BufferedReader(new FileReader(args[0]));
         FileWriter fileWriter = new FileWriter(args[1]);
@@ -23,9 +50,10 @@ public class Solution {
         }
 
         for (int i = 0; i < list.size(); i++)
-            fileWriter.write(!list.get(i).equals(list.get(list.size() - 1)) ? list.get(i) + ", " : list.get(i));
+            fileWriter.write(!list.get(i).equals(list.get(list.size() - 1)) ? list.get(i) + "," : list.get(i));
 
         fileReader.close();
         fileWriter.close();
     }
 }
+*/
