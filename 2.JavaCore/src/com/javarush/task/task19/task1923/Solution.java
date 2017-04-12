65ffe591f8167657385b905117fc2115ab992d47
roninout <roninorgua@gmail.com>
2017-04-12 18:02:09 +0300

    05.04.2017

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1923/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task19.task1923;
+
+/* 
+Слова с цифрами
+*/
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader fileReader = new BufferedReader(new FileReader(args[0]));
+        FileWriter fileWriter = new FileWriter(args[1]);
+
+        while (fileReader.ready()){
+            String [] word = fileReader.readLine().split(" ");
+            for (String anWord : word) {
+                Pattern p = Pattern.compile("\\d+\\w+");
+                Matcher m = p.matcher(anWord);
+                if (m.matches())
+                    fileWriter.write(anWord + " ");
+            }
+        }
+        fileReader.close();
+        fileWriter.close();
+    }
+}
