0211babc2c87605c86eb6b95b6dcd95f2bdfd84d
roninout <roninorgua@gmail.com>
2017-04-12 11:05:37 +0300

    05.04.2017

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1924/Solution.java
@@ -0,0 +1,58 @@
+package com.javarush.task.task19.task1924;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Замена чисел
+*/
+
+public class Solution {
+    public static Map<Integer, String> map = new HashMap<>();
+    static {
+        map.put(0, "ноль");
+        map.put(1, "один");
+        map.put(2, "два");
+        map.put(3, "три");
+        map.put(4, "четыре");
+        map.put(5, "пять");
+        map.put(6, "шесть");
+        map.put(7, "семь");
+        map.put(8, "восемь");
+        map.put(9, "девять");
+        map.put(10, "десять");
+        map.put(11, "одиннадцать");
+        map.put(12, "двенадцать");
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()));
+
+        while (fileReader.ready()){
+            String [] word = fileReader.readLine().split(" ");
+            String result = "";
+
+            for (String anWord : word) {
+                try {
+                    Integer numb = Integer.parseInt(anWord);
+                    if (map.containsKey(numb))
+                        result += map.get(numb) + " ";
+                    else
+                        result += anWord + " ";
+                }
+                catch (Exception e) { result += anWord + " "; }
+            }
+            System.out.println(result);
+        }
+
+        reader.close();
+        fileReader.close();
+    }
+}
