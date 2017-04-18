2ba8c48865922b69037cec4bf784741feeb30f84
roninout <roninorgua@gmail.com>
2017-04-18 16:13:49 +0300

    05.04.2017

--- 2.JavaCore/src/com/javarush/task/task20/task2006/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2006/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task20.task2006;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,14 +9,14 @@ import java.util.List;
 Как сериализовать?
 */
 public class Solution {
-    public static class Human {
+    public static class Human implements Serializable{
         public String name;
         public List<Asset> assets = new ArrayList<>();
 
         public Human() {
         }
 
-        public Human(String name, Asset... assets) {
+        public Human(String name, Asset... assets){
             this.name = name;
             if (assets != null) {
                 this.assets.addAll(Arrays.asList(assets));
