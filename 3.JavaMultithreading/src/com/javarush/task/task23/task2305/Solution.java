374dd2315bd529b4389af527aae0a3e6ecfc6992
alexanderkovalchuk <roninorgua@gmail.com>
2017-05-07 22:04:14 +0300

    2017_04_10

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task23.task2305;
+
+/* 
+Inner
+*/
+public class Solution {
+    public InnerClass[] innerClasses = new InnerClass[2];
+
+    public class InnerClass {
+    }
+
+    public static Solution[] getTwoSolutions() {
+        Solution solution1 = new Solution();
+        Solution solution2 = new Solution();
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
