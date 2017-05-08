eb6d95128d11e12309feece37fcb1944cd0c3357
alexanderkovalchuk <roninorgua@gmail.com>
2017-05-08 23:46:09 +0300

    2017_04_10

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarker.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task24.task2401;
+
+/**
+ * Created by alexanderkovalchuk on 08.05.17.
+ */
+public interface SelfInterfaceMarker {
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task24.task2401;
+
+/**
+ * Created by alexanderkovalchuk on 08.05.17.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker{
+    public void methodOne(){}
+
+    public void methodTwo(){}
+}
