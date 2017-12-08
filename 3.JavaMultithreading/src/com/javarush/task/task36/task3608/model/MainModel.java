985abd9e2e731444010224b35c0bf73d88ff384f
roninout <roninorgua@gmail.com>
2017-12-08 15:57:19 +0200

    07.12.2017

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -0,0 +1,53 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.model.service.UserService;
+import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+import java.util.List;
+
+
+public class MainModel implements Model{
+    private ModelData modelData = new ModelData();
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        modelData.setDisplayDeletedUserList(false);
+        modelData.setUsers(getAllUsers());
+    }
+
+    private List<User> getAllUsers() {
+        return userService.filterOnlyActiveUsers(userService.getUsersBetweenLevels(1, 100));
+    }
+
+    @Override
+    public void loadDeletedUsers() {
+        modelData.setDisplayDeletedUserList(true);
+        List<User> users = userService.getAllDeletedUsers();
+        modelData.setUsers(users);
+    }
+
+    @Override
+    public void loadUserById(long userId) {
+        modelData.setActiveUser(userService.getUsersById(userId));
+    }
+
+    @Override
+    public void deleteUserById(long id) {
+        userService.deleteUser(id);
+        loadUsers();
+    }
+
+    @Override
+    public void changeUserData(String name, long id, int level) {
+        userService.createOrUpdateUser(name, id, level);
+        loadUsers();
+    }
+
+}
--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/Model.java
@@ -3,4 +3,8 @@ package com.javarush.task.task36.task3608.model;
 public interface Model {
     ModelData getModelData();
     void loadUsers();
+    void loadDeletedUsers();
+    void loadUserById(long userId);
+    void deleteUserById(long id);
+    void changeUserData(String name, long id, int level);
 }
